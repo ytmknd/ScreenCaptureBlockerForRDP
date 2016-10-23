b058f2560dee1179d0650a6b860d6658cdce7a6e
ytmknd <yagit@mknada.sakura.ne.jp>
2016-10-23 14:49:22 +0900

    init

---
+++ HookDLL/dllmain.cpp
@@ -0,0 +1,66 @@
+// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
+#include "stdafx.h"
+
+#include <iostream>
+#include <fstream>
+
+#include "HookDLL.h"
+
+using namespace std;
+
+#pragma data_seg("hookdll")
+HWND hWnd = nullptr;
+DWORD pid = 0;
+#pragma data_seg()
+
+HINSTANCE hInst = nullptr;
+HHOOK hHook = nullptr;
+
+LRESULT CALLBACK cbtProc(int code, WPARAM wParam, LPARAM lParam) {
+	if (code > 0) {
+		if (code == HCBT_ACTIVATE) {
+			TCHAR buf[128];
+			if (GetClassName((HWND)wParam, (LPWSTR)buf, 128)) {
+				if (!wcscmp(L"TscShellContainerClass", buf) // RDS Window class
+					|| !wcscmp(L"RAIL_WINDOW", buf)) {		// RemoteApp Window class
+					SetWindowDisplayAffinity((HWND)wParam, WDA_MONITOR);
+				}
+			}
+		}
+	}
+	return CallNextHookEx(hHook, code, wParam, lParam);
+}
+
+DLLHOOKPREF bool InstallHook(HWND _hWnd) {
+	if (hHook) return false;
+	hWnd = _hWnd;
+	GetWindowThreadProcessId(hWnd, &pid);
+	return (hHook = SetWindowsHookEx(WH_CBT, cbtProc, hInst, 0)) != NULL;
+}
+
+DLLHOOKPREF bool UninstallHook() {
+	if (!hHook) return false;
+	bool res = UnhookWindowsHookEx(hHook) != NULL;
+	if (res) hHook = NULL;
+	return res;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+		hInst = reinterpret_cast<HINSTANCE>(hModule);
+		break;
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		hInst = nullptr;
+		break;
+	}
+	return TRUE;
+}
+
---
+++ ScreenCaptureBlockerForRDP.cpp
@@ -0,0 +1,273 @@
+﻿// ScreenCaptureBlockerForRDP.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <shellapi.h>
+#include <Dwmapi.h>
+#include <strsafe.h>
+#include "ScreenCaptureBlockerForRDP.h"
+#include "HookDLL/HookDLL.h"
+
+#define MAX_LOADSTRING 100
+
+// グローバル定数:
+UINT const WMAPP_NOTIFYCALLBACK = WM_APP + 1;
+
+// グローバル変数:
+HINSTANCE hInst;                                // 現在のインターフェイス
+WCHAR szTitle[MAX_LOADSTRING];                  // タイトル バーのテキスト
+WCHAR szWindowClass[MAX_LOADSTRING];            // メイン ウィンドウ クラス名
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void                ShowContextMenu(HWND hwnd, POINT pt);
+BOOL                AddNotificationIcon(HWND hwnd);
+BOOL                DeleteNotificationIcon();
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: ここにコードを挿入してください。
+
+    // グローバル文字列を初期化しています。
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // アプリケーションの初期化を実行します:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    MSG msg;
+
+    // メイン メッセージ ループ:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
+}
+
+//
+//  関数: MyRegisterClass()
+//
+//  目的: ウィンドウ クラスを登録します。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DENYTAKINGSCREENCAPTUREFORRDP));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DENYTAKINGSCREENCAPTUREFORRDP);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   関数: InitInstance(HINSTANCE, int)
+//
+//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します。
+//
+//   コメント:
+//
+//        この関数で、グローバル変数でインスタンス ハンドルを保存し、
+//        メイン プログラム ウィンドウを作成および表示します。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // グローバル変数にインスタンス処理を格納します。
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   InstallHook(hWnd);
+
+   return TRUE;
+}
+
+//
+//  関数: AddNotificationIcon()
+//
+//  目的: 通知領域にインジケーターアイコンを表示します。
+//
+BOOL AddNotificationIcon(HWND hwnd)
+{
+	NOTIFYICONDATA nid = { sizeof(nid) };
+	nid.hWnd = hwnd;
+	// add the icon, setting the icon, tooltip, and callback message.
+	// the icon will be identified with the GUID
+	nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE | NIF_SHOWTIP | NIF_GUID;
+	nid.uCallbackMessage = WMAPP_NOTIFYCALLBACK;
+	nid.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_SMALL));
+	LoadString(hInst, IDS_TOOLTIP, nid.szTip, ARRAYSIZE(nid.szTip));
+	Shell_NotifyIcon(NIM_ADD, &nid);
+
+	// NOTIFYICON_VERSION_4 is prefered
+	nid.uVersion = NOTIFYICON_VERSION_4;
+	return Shell_NotifyIcon(NIM_SETVERSION, &nid);
+}
+
+//
+//  関数: DeleteNotificationIcon()
+//
+//  目的: 通知領域からインジケーターアイコンを削除します。
+//
+BOOL DeleteNotificationIcon()
+{
+	NOTIFYICONDATA nid = { sizeof(nid) };
+	nid.uFlags = NIF_GUID;
+	return Shell_NotifyIcon(NIM_DELETE, &nid);
+}
+
+//
+//  関数: ShowContextMenu()
+//
+//  目的: 
+//
+void ShowContextMenu(HWND hwnd, POINT pt)
+{
+	HMENU hMenu = LoadMenu(hInst, MAKEINTRESOURCE(IDC_CONTEXTMENU));
+	if (hMenu)
+	{
+		HMENU hSubMenu = GetSubMenu(hMenu, 0);
+		if (hSubMenu)
+		{
+			// our window must be foreground before calling TrackPopupMenu or the menu will not disappear when the user clicks away
+			SetForegroundWindow(hwnd);
+
+			// respect menu drop alignment
+			UINT uFlags = TPM_RIGHTBUTTON;
+			if (GetSystemMetrics(SM_MENUDROPALIGNMENT) != 0)
+			{
+				uFlags |= TPM_RIGHTALIGN;
+			}
+			else
+			{
+				uFlags |= TPM_LEFTALIGN;
+			}
+
+			TrackPopupMenuEx(hSubMenu, uFlags, pt.x, pt.y, hwnd, NULL);
+		}
+		DestroyMenu(hMenu);
+	}
+}
+
+//
+//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的:    メイン ウィンドウのメッセージを処理します。
+//
+//  WM_COMMAND  - アプリケーション メニューの処理
+//  WM_PAINT    - メイン ウィンドウの描画
+//  WM_DESTROY  - 中止メッセージを表示して戻る
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE:
+		// add the notification icon
+		if (!AddNotificationIcon(hWnd))
+		{
+			MessageBox(hWnd,
+				L"Please read the ReadMe.txt file for troubleshooting",
+				L"Error adding icon", MB_OK);
+			return -1;
+		}
+		break;
+	case WM_COMMAND:
+	{
+		int const wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			// placeholder for an options dialog
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+
+	case WMAPP_NOTIFYCALLBACK:
+		switch (LOWORD(lParam))
+		{
+		case NIN_SELECT:
+			// NIN_SELECT is prerable to listening to mouse clicks and key presses directly.
+			//MessageBox(hWnd, L"Display the options dialog here.", L"Options", MB_OK); // Do Nothing
+			break;
+
+		case WM_CONTEXTMENU:
+		{
+			POINT const pt = { LOWORD(wParam), HIWORD(wParam) };
+			ShowContextMenu(hWnd, pt);
+		}
+		break;
+}
+		break;
+
+	case WM_DESTROY:
+		UninstallHook();
+		DeleteNotificationIcon();
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
